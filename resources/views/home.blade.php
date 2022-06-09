2076dc4391b0fb7784d91da4302aa3c7b3e7bac1
Nicolas De Palma <wellcomenicolas@gmail.com>
2022-06-09 16:55:47 +0200

    Add first part of header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,9 @@
 @include ('layouts.tag_head')
 
+@include ('layouts.tag_main')
 
+@include ('partials.header')
+
+@include ('partials.main')
 
 @include ('layouts.tag_footer')
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,29 @@
+<header>
+
+    <div class="bg-primary text-white">
+        <div class="powered-visa container">
+            <span>POWERED Visa</span>
+            <span>addictional series</span>
+        </div>
+    </div>
+    <nav>
+        <div class="container">
+            <img src="/../resources/img/dc-logo-png" alt="...">
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Active</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+        </div>
+
+    </nav>
+</header>
