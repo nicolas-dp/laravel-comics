941213d6b83fb78def121d50b32c074c07e10e84
Nicolas De Palma <wellcomenicolas@gmail.com>
2022-06-10 17:07:23 +0200

    Add css on single card

--- resources/views/cards/show.blade.php
+++ resources/views/cards/show.blade.php
@@ -7,14 +7,36 @@
     <div class="container py-5">
 
         <div class="row">
-            <div class="col">
-                <img width="600" src="{{$card['thumb']}}" alt="">
+            <div class="col text-center">
+                <img width="320" height="430" src="{{$card['thumb']}}" alt="">
             </div>
             <div class="col">
                 <div class="product_title d-flex">
-                    <img width="50" src="{{$card['thumb']}}" alt="">
-                    <h1 class="pb-2">{{$card['title']}}</h1>
-                </div>                
+                    <img width="80" height="100" src="{{$card['thumb']}}" alt="">
+                    <div class="text_card ms-2">
+                        <h1>{{$card['title']}}</h1>
+                        <small>({{$card['series']}})</small>
+                    </div>
+
+                </div>
+                <div class="content">
+                    {!! $card['description'] !!}
+                </div>
+                <hr>
+                <div class="metadata">
+                    <span class="text-uppercase">
+                        <strong>Tipo: </strong>
+                        {{$card['type']}}
+                    </span>
+                    <span>
+                        <strong>Prezzo: </strong>
+                        {{$card['price']}}
+                    </span>
+                    <span>
+                        <strong>Sale Date: </strong>
+                        {{$card['sale_date']}}
+                    </span>
+                </div>
             </div>
         </div>
     </div>
